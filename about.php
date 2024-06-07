00d880dee915d7db4dd1b8d8674409c27b3be04f
EvGen Gal <genwolk1@gmail.com>
2024-06-07 19:59:54 +0300

    +form

--- about.php
+++ about.php
@@ -2,7 +2,18 @@
 $title = "О Нас";
 require("blocks/header.php");
 ?>
-<h1>О Нас</h1>
+<div class="container mt-2">
+  <h1>О Нас</h1>
+
+  <form action="check_post.php" method="post">
+    <input autocomplete="off" type="text" name="username" placeholder="Введите имя" class="form-control"><br />
+    <input autocomplete="off" type="email" name="email" placeholder="Введите email" class="form-control"><br />
+    <input autocomplete="off" type="password" name="password" placeholder="Введите пароль" class="form-control"><br />
+    <textarea name="message" class="form-control" placeholder="Введите смс"></textarea><br />
+    <input type="submit" value="Отправить" class="btn btn-success">
+  </form>
+</div>
+
 <?php
 require("blocks/footer.php");
 ?>
