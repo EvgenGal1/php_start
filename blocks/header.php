fc8e341a101279fe90fd5033ceada4e76628ce96
EvGen Gal <genwolk1@gmail.com>
2024-06-08 03:56:06 +0300

    #header(head,stl,logo)

--- blocks/header.php
+++ blocks/header.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='ru'>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <link rel="stylesheet" href="./styles/style.css" />
   <title><?= $title ?></title>
 </head>
 
-<body>
-  <header>
-    <nav>
+<body style="margin: 0; padding: 0;">
+  <header style="display:flex; justify-content: space-between; align-items: center; padding: 5px 10px; background-color: #eeeeee">
+    <div style="background-color: #cccccc;">
+      <a href="#" style="
+          display: block; 
+          width: 25px; height: 25px;
+          border: 5px solid darkred;
+          border-top-color: gold;
+          border-top-right-radius: 50px;
+          border-bottom-color: gold;
+          border-bottom-left-radius: 50px;
+          background-color: #777777">
+        <img src="#" alt="">
+      </a>
+    </div>
+    <nav style="font-size: 20px; font-weight: bold;">
       <a href="index.php" title="1">Главная</a> |
       <a href="about.php" title="2">Про Нас</a>
     </nav>
